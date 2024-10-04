211e17473f6dfc5fb59304a7f0d4f3cee6fdf70d
Hamraouii <aelhamraoui748@gmail.com>
2024-10-04 23:11:56 +0100

    prod

--- src/environment.ts
+++ src/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
-  //apiUrl: `https://autoecole.hafidi.ma/api`  
-  apiUrl: `http://localhost:8080/api`
+  apiUrl: `https://autoecole.hafidi.ma/api`  
+  //apiUrl: `http://localhost:8080/api`
 };
